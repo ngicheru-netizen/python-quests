7c6ef249be3d403a7fcd2d92fe657cef1adeb209
sam-hez <h.epodoi@alustudent.com>
2026-02-25 11:21:26 -0800

    Quest 6 Fortune Teller

---
+++ quests/quest_06_fortune_teller.py
@@ -0,0 +1,9 @@
+#quest 6: the fortune teller
+
+# this is a simple program that simulates a fortune teller. 
+# it asks the user for their name and their quest, and then gives them a fortune based on their input.
+
+user_name = input("\nWhat is your name? ")
+user_quest = input("What is your quest? ")
+
+print(f"\n=============\n ahh!, {user_name}, you seek to explore {user_quest}. I see a great adventure in your future! \n ===========\n")
